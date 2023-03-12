6f3445f5826e699a51270f5c0ac655978e3fce38
xanimal37 <hawkbit37@gmail.com>
2023-03-11 22:42:59 -0600

    updated front page stats

--- JPAGoalie/src/main/java/com/skilldistillery/goalie/entities/Shot.java
+++ JPAGoalie/src/main/java/com/skilldistillery/goalie/entities/Shot.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.goalie.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,16 +15,24 @@ public class Shot {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
+	@Column(name = "shot_id")
+	private int shotId;
 	private boolean screen;
-
-	public int getId() {
-		return id;
+	private boolean goal;
+	@Column(name="save_selection")
+	private String saveSelection;
+	private boolean low;
+	private String zone;
+	private String note;
+	private String situation;
+	private boolean rebound;
+	
+	public int getShotId() {
+		return shotId;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setShotId(int shotId) {
+		this.shotId= shotId;
 	}
 
 	public boolean isScreen() {
@@ -33,11 +42,71 @@ public class Shot {
 	public void setScreen(boolean screen) {
 		this.screen = screen;
 	}
+	
+	public boolean isGoal() {
+		return goal;
+	}
+	
+	public void setGoal(boolean goal) {
+		this.goal = goal;
+	}
+
+	public String getSaveSelection() {
+		return saveSelection;
+	}
+
+	public void setSaveSelection(String saveSelection) {
+		this.saveSelection = saveSelection;
+	}
+
+	public boolean isLow() {
+		return low;
+	}
+
+	public void setLow(boolean low) {
+		this.low = low;
+	}
+
+	public String getZone() {
+		return zone;
+	}
+
+	public void setZone(String zone) {
+		this.zone = zone;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public String getSituation() {
+		return situation;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+
+	public boolean isRebound() {
+		return rebound;
+	}
+
+	public void setRebound(boolean rebound) {
+		this.rebound = rebound;
+	}
 
 	@Override
 	public String toString() {
-		return "Shot [id=" + id + ", screen=" + screen + "]";
+		return "Shot [shotId=" + shotId + ", screen=" + screen + ", goal=" + goal + ", saveSelection=" + saveSelection
+				+ ", low=" + low + ", zone=" + zone + ", note=" + note + ", situation=" + situation + ", rebound="
+				+ rebound + "]";
 	}
+
+	
 
 	
 	
--- JPAGoalie/src/test/java/com/skilldistillery/goalie/entities/ShotTest.java
+++ JPAGoalie/src/test/java/com/skilldistillery/goalie/entities/ShotTest.java
@@ -3,6 +3,8 @@ package com.skilldistillery.goalie.entities;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -46,7 +48,19 @@ class ShotTest {
 	@Test
 	void test_shot_entity_from_db() {
 		assertNotNull(shot);
+		System.out.println(shot);
 		assertEquals(false,shot.isScreen());
+	}
+	
+	@Test
+	void test_shot_entity_mappings() {
+		assertNotNull(shot);
+		String query = "SELECT s FROM Shot s";
+		List<Shot> shots = em.createQuery(query,Shot.class).getResultList();
+		for(Shot s : shots) {
+			System.out.println(s);
+		}
+		assertEquals(10,shots.size());
 	}
 
 }
